94895a6e0a6e1da238e1dbf3761327bad856ab2f
jubayer50 <aujubayer1@gmail.com>
2026-03-30 17:20:31 +0600

    condition ways css apply on pricing card

--- src/Components/PricingOptions/PricingCard.jsx
+++ src/Components/PricingOptions/PricingCard.jsx
@@ -4,12 +4,20 @@ import PricingFeatures from "./PricingFeatures";
 const PricingCard = ({ pricingPlan }) => {
   console.log(pricingPlan);
   return (
-    <div className="p-6 rounded-2xl flex flex-col bg-[#F9FAFC] border-2 border-[#F2F2F2] relative">
+    <div
+      className={`p-6 rounded-2xl flex flex-col ${pricingPlan.highlighted ? "text-white bg-linear-to-r from-[#4F39F6] to-[#9514FA]" : "bg-[#F9FAFC]"} border-2 border-[#F2F2F2] relative`}
+    >
       <div>
-        <h3 className="text-2xl text-[#101727] font-bold">
+        <h3
+          className={`text-2xl ${pricingPlan.highlighted ? "text-white" : "text-[#101727]"} font-bold`}
+        >
           {pricingPlan.name}
         </h3>
-        <p className="text-[#627382] mt-2">{pricingPlan.best_for}</p>
+        <p
+          className={`${pricingPlan.highlighted ? "text-white" : "text-[#627382]"} mt-2`}
+        >
+          {pricingPlan.best_for}
+        </p>
       </div>
 
       {pricingPlan.highlighted && (
@@ -19,8 +27,12 @@ const PricingCard = ({ pricingPlan }) => {
       )}
 
       <div className="my-6">
-        <h2 className="text-[#627382] text-[20px]">
-          <span className="text-[40px] font-bold text-[#101727]">
+        <h2
+          className={`${pricingPlan.highlighted ? "text-white" : "text-[#627382]"} text-[20px]`}
+        >
+          <span
+            className={`text-[40px] font-bold ${pricingPlan.highlighted ? "text-white" : "text-[#101727]"}`}
+          >
             ${pricingPlan.price}
           </span>
           /Month
